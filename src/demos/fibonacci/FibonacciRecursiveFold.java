a76f3c001f8affe2401b17f1534fe13390a7db70
FranciscoRibeiro <kikoribeiro95@gmail.com>
2018-06-21 09:48:16 +0100

    Add demos for fibonacci using tuples and for fibonacci using recursive fold

---
+++ src/demos/fibonacci/FibonacciRecursiveFold.java
@@ -0,0 +1,48 @@
+package demos.fibonacci;
+
+import util.Either;
+import util.Left;
+import util.Pair;
+import util.Right;
+
+import java.lang.invoke.MethodHandles;
+import java.math.BigInteger;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static datatypes.FStream.unfoldrBT;
+
+public class FibonacciRecursiveFold extends MasterBenchmarkFibonacci{
+    public static void main(String[] args) {
+        System.out.println(MethodHandles.lookup().lookupClass().getSimpleName() + "...");
+
+        FibonacciRecursiveFold fibRF = new FibonacciRecursiveFold();
+
+        /*fibRF.populate();
+
+        fibRF.warmUp();*/
+
+        fibRF.measure();
+
+        fibRF.end();
+    }
+
+    @Override
+    public void work() {
+        Function<Integer, Either<BigInteger, Pair<Integer,Integer>>> g = x -> {
+            if (x == 0){
+                return new Left<>(BigInteger.ZERO);
+            }
+            else if(x == 1){
+                return new Left<>(BigInteger.ONE);
+            }
+            else{
+                return new Right<>(new Pair<>(x-1, x-2));
+            }
+        };
+
+        BiFunction<BigInteger, BigInteger, BigInteger> sum = (x, y) -> x.add(y);
+
+        System.out.println(unfoldrBT(g, 45).foldBT(sum, Function.identity()));
+    }
+}
---
+++ src/demos/fibonacci/FibonacciTuple.java
@@ -0,0 +1,46 @@
+package demos.fibonacci;
+
+import util.Pair;
+
+import java.lang.invoke.MethodHandles;
+import java.math.BigInteger;
+
+public class FibonacciTuple extends MasterBenchmarkFibonacci{
+
+    private static BigInteger fib(int n){
+        //return second element of tuple
+        return fibAux(n).getY();
+    }
+
+    private static Pair<BigInteger,BigInteger> fibAux(int n){
+        if(n == 1 || n == 2){
+            return new Pair<>(BigInteger.valueOf(1), BigInteger.valueOf(1));
+        }
+        else{
+            Pair<BigInteger, BigInteger> p = fibAux(n-1);
+            BigInteger n2 = p.getX();
+            BigInteger n1 = p.getY();
+            return new Pair<>(n1, n2.add(n1));
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MethodHandles.lookup().lookupClass().getSimpleName() + "...");
+
+        FibonacciTuple fibT = new FibonacciTuple();
+
+        /*fibT.populate();
+
+        fibT.warmUp();*/
+
+        fibT.measure();
+
+        fibT.end();
+    }
+
+    @Override
+    public void work() {
+        BigInteger fib = fib(45);
+        System.out.println(fib);
+    }
+}
